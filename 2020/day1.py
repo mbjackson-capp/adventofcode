6f480d864afea4299a41690b541f7026509fc04c
Matthew Jackson <mbjackson@Matthews-MacBook-Air.local>
2023-05-13 00:44:20 -0500

    day 1 2020

---
+++ 2020/day1.py
@@ -0,0 +1,24 @@
+from aocd import get_data
+#set session token first - https://github.com/wimglenn/advent-of-code-wim/issues/1
+import re
+
+#Problem statement: https://adventofcode.com/2020/day/1
+
+input = [int(i) for i in get_data(day=1, year=2020).split('\n')]
+
+def part1():
+    for i, num in enumerate(input):
+        for j in range(i+1, len(input)):
+            if num + input[j] == 2020:
+                return num * input[j]
+
+def part2():
+    for i, num in enumerate(input):
+        for j in range(i+1, len(input)): 
+            for k in range(j+1, len(input)):
+                if num + input[j] + input[k] == 2020:
+                    return num * input[j] * input[k]
+
+if __name__ == '__main__':
+    print(part1())
+    print(part2())
