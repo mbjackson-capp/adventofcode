68abff21ca3cdedb4ee9d8de8e45b4cd1d35d194
mbjackson-capp <mbjackson@uchicago.edu>
2025-12-11 16:39:27 -0600

    2025 day11 cleaned up

--- 2025/day11.py
+++ 2025/day11.py
@@ -23,6 +23,8 @@ def parse_input(data: str) -> nx.DiGraph:
 
 
 def draw_network(G, filepath: str) -> None:
+    """Create an image of the network and save to folder. For debug purposes only;
+    wraps around pretty basic matplotlib and networkx functionality"""
     fig = plt.figure(figsize=(40, 18))
     ax = plt.subplot(111)
     ax.set_title("Advent of Code 2025 Day 11 Visualization", fontsize=10)
@@ -60,34 +62,12 @@ def num_paths_between(G: nx.DiGraph, source, target) -> int:
         )
 
 
-def valid_paths_network(G: nx.DiGraph, source="svr", target="out") -> nx.DiGraph:
-    """Prune nodes that aren't on a valid path. This turns out to be useless
-    with provided input since every node is on a valid path"""
-    toposort = list(nx.topological_sort(G))
-    print(f"'dac' at index {toposort.index(DAC)}, 'fft' at index {toposort.index(FFT)}")
-    dac_ix = toposort.index(DAC)
-    fft_ix = toposort.index(FFT)
-    mid_former_ix = min(dac_ix, fft_ix)
-    mid_latter_ix = max(dac_ix, fft_ix)
-    to_delete = []
-    for i, node in enumerate(toposort[:mid_former_ix]):
-        if not nx.has_path(G, source, toposort[mid_former_ix]):
-            to_delete.append(node)
-    for j, node in enumerate(toposort[mid_former_ix:mid_latter_ix]):
-        if not nx.has_path(G, source, toposort[mid_latter_ix]):
-            to_delete.append(node)
-    for node in to_delete:
-        G.remove_node(node)
-    return G
-
-
 def part2(G: nx.DiGraph, source: str = "svr", target: str = "out"):
-    if nx.has_path(G, DAC, FFT):
-        mid_former, mid_latter = (DAC, FFT)
-    elif nx.has_path(G, FFT, DAC):
-        mid_former, mid_latter = (FFT, DAC)
-    else:
-        return 0
+    # determine which of 'dac' or 'fft' must come first in valid paths
+    toposort = list(nx.topological_sort(G))
+    mid_former, mid_latter = (
+        (DAC, FFT) if toposort.index(DAC) < toposort.index(FFT) else (FFT, DAC)
+    )
     return (
         num_paths_between(G, source, mid_former)
         * num_paths_between(G, mid_former, mid_latter)
